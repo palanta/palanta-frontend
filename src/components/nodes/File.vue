459f3c558ea8339f0814c9f82d648bd51bd73c18
Juli <juliane2109@gmail.com>
2019-12-09 18:04:53 +0100

    Add File node

---
+++ src/components/nodes/File.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="q-px-md q-pt-md">
+    <q-input @input="val => { file = val[0] }"
+      filled
+      type="file"
+      accept=".jpg, image/*"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  spec: {
+    id: 'std::image',
+    title: 'Image',
+    inputs: [],
+    outputs: [
+      {
+        name: 'Image',
+        type: 'image'
+      }
+    ]
+  },
+  data () {
+    return {
+      file: null
+    }
+  }
+}
+</script>
