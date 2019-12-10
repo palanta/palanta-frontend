ebdcc0aca7b1d93cc9fc419172740a0ceec6bceb
AKoll <hsv-fan_anton@hotmail.de>
2019-12-10 22:54:17 +0100

    Fix old connect event field being used for variadic updates

--- src/components/Canvas.vue
+++ src/components/Canvas.vue
@@ -153,8 +153,8 @@ export default {
           if (this.newEdge.start) this.newEdge.start.connecting = false
           if (this.newEdge.end) this.newEdge.end.connecting = false
           this.addEdge(this.newEdge)
-          event.instance.node.updateVariadics()
-          if (this.newEdge.start.node && event.instance.node !== this.newEdge.start.node) {
+          event.component.node.updateVariadics()
+          if (this.newEdge.start.node && event.component.node !== this.newEdge.start.node) {
             this.newEdge.start.node.updateVariadics()
           }
           this.newEdge = null
