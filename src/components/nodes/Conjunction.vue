9c28f74461322a7dc7f628ed4f9f61dd1f77fc58
Juli <juliane2109@gmail.com>
2019-12-11 00:51:48 +0100

    Add conjunction node

---
+++ src/components/nodes/Conjunction.vue
@@ -0,0 +1,28 @@
+<template>
+  <div/>
+</template>
+
+<script>
+export default {
+  spec: {
+    id: 'std::conjunction',
+    title: 'Conjunction',
+    icon: require('../../assets/node_icons/node_conjunction.svg'),
+    inputs: [
+      {
+        id: 'boolean',
+        name: 'Boolean',
+        type: 'boolean',
+        variadic: true
+      }
+    ],
+    outputs: [
+      {
+        id: 'boolean',
+        name: 'Boolean',
+        type: 'boolean'
+      }
+    ]
+  }
+}
+</script>
