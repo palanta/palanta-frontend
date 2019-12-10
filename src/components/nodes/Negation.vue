51171b111ef5d89cc258590bc0ead13c573d9bba
Juli <juliane2109@gmail.com>
2019-12-11 00:56:33 +0100

    Add negation node

---
+++ src/components/nodes/Negation.vue
@@ -0,0 +1,27 @@
+<template>
+  <div/>
+</template>
+
+<script>
+export default {
+  spec: {
+    id: 'std::negation',
+    title: 'Negation',
+    icon: require('../../assets/node_icons/node_negation.svg'),
+    inputs: [
+      {
+        id: 'boolean',
+        name: 'Boolean',
+        type: 'boolean'
+      }
+    ],
+    outputs: [
+      {
+        id: 'boolean',
+        name: 'Boolean',
+        type: 'boolean'
+      }
+    ]
+  }
+}
+</script>
