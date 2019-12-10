9922f4a9b2447e0f0bef0ff87c04a7b29d380311
Juli <juliane2109@gmail.com>
2019-12-11 00:57:53 +0100

    Add exclusive disjuncion node

---
+++ src/components/nodes/ExclusiveDisjunction.vue
@@ -0,0 +1,28 @@
+<template>
+  <div/>
+</template>
+
+<script>
+export default {
+  spec: {
+    id: 'std::exclusivedisjunction',
+    title: 'Exclusive Disjunction',
+    icon: require('../../assets/node_icons/node_exclusivedisjunction.svg'),
+    inputs: [
+      {
+        id: 'boolean',
+        name: 'Boolean',
+        type: 'boolean',
+        variadic: true
+      }
+    ],
+    outputs: [
+      {
+        id: 'boolean',
+        name: 'Boolean',
+        type: 'boolean'
+      }
+    ]
+  }
+}
+</script>
