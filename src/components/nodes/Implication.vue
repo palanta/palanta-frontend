c30d7a8666f2c0fbbc04c7985064e54c3fd497b1
Juli <juliane2109@gmail.com>
2019-12-11 00:58:58 +0100

    Add implication node

---
+++ src/components/nodes/Implication.vue
@@ -0,0 +1,32 @@
+<template>
+  <div/>
+</template>
+
+<script>
+export default {
+  spec: {
+    id: 'std::implication',
+    title: 'Implication',
+    icon: require('../../assets/node_icons/node_implication.svg'),
+    inputs: [
+      {
+        id: 'boolean',
+        name: 'Boolean',
+        type: 'boolean'
+      },
+      {
+        id: 'boolean',
+        name: 'Boolean',
+        type: 'boolean'
+      }
+    ],
+    outputs: [
+      {
+        id: 'boolean',
+        name: 'Boolean',
+        type: 'boolean'
+      }
+    ]
+  }
+}
+</script>
