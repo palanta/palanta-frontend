118554c00b377e801cc21013ad01fc9c97da4b98
Juli <juliane2109@gmail.com>
2019-12-11 14:18:30 +0100

    add logical.js

---
+++ src/components/nodes/logical/logical.js
@@ -0,0 +1,15 @@
+import PNConjunction from './Conjunction'
+import PNDisjunction from './Disjunction'
+import PNExclusiveDisjunction from './ExclusiveDisjunction'
+import PNNegation from './Negation'
+import PNImplication from './Implication'
+import PNBiconditional from './Biconditional'
+
+export default {
+  PNConjunction,
+  PNDisjunction,
+  PNExclusiveDisjunction,
+  PNNegation,
+  PNImplication,
+  PNBiconditional
+}
