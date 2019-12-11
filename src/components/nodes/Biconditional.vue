ec03278bbe5d1ca5fd2cebcda239e0039b09812f
Juli <juliane2109@gmail.com>
2019-12-11 01:00:08 +0100

    Add biconditional node

---
+++ src/components/nodes/Biconditional.vue
@@ -0,0 +1,32 @@
+<template>
+  <div/>
+</template>
+
+<script>
+export default {
+  spec: {
+    id: 'std::biconditional',
+    title: 'Biconditional',
+    icon: require('../../assets/node_icons/node_biconditional.svg'),
+    inputs: [
+      {
+        id: 'boolean',
+        name: 'Boolean',
+        type: 'boolean'
+      },
+      {
+        id: 'boolean',
+        name: 'Boolean',
+        type: 'boolean'
+      }
+    ],
+    outputs: [
+      {
+        id: 'boolean',
+        name: 'Boolean',
+        type: 'boolean'
+      }
+    ]
+  }
+}
+</script>
