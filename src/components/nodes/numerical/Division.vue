5b833b4c396cac5c5fb8f0cfd29abc77985c38f6
Juli <juliane2109@gmail.com>
2020-01-14 19:01:26 +0100

    Implement Division node functionality

--- src/components/nodes/numerical/Division.vue
+++ src/components/nodes/numerical/Division.vue
@@ -11,23 +11,32 @@ export default {
     icon: require('../../../assets/node_icons/node_division.svg'),
     inputs: [
       {
-        id: 'dividend',
-        name: 'Dividend',
-        type: 'number'
-      },
-      {
-        id: 'divisor',
-        name: 'Divisor',
+        id: 'number',
+        name: 'Number',
         type: 'number',
-        variadic: true
+        variadic: {
+          minimum: 2
+        }
       }
     ],
     outputs: [
       {
+        id: 'quotient',
         name: 'Quotient',
         type: 'number'
       }
-    ]
+    ],
+    calculate () {
+      let quotient = 0
+      const factors = this.input('number').map(factor => factor.value).filter(factor => !isNaN(factor))
+      if (factors.length > 0) {
+        quotient = factors.shift()
+        factors.forEach(dividor => {
+          quotient /= dividor
+        })
+      }
+      this.output('quotient').value = quotient
+    }
   }
 }
 </script>
