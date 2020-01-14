0f2d3b7fdbb44d65526c75dc5e60a2c5d8e5eca0
Juli <juliane2109@gmail.com>
2020-01-14 19:29:06 +0100

    Implement Logarithm node functionality

--- src/components/nodes/numerical/Logarithm.vue
+++ src/components/nodes/numerical/Logarithm.vue
@@ -11,20 +11,33 @@ export default {
     icon: require('../../../assets/node_icons/node_logarithm.svg'),
     inputs: [
       {
+        id: 'number',
         name: 'Number',
         type: 'number'
       },
       {
+        id: 'base',
         name: 'Base',
         type: 'number'
       }
     ],
     outputs: [
       {
+        id: 'logarithm',
         name: 'Logarithm',
         type: 'number'
       }
-    ]
+    ],
+    calculate () {
+      let logarithm = 0
+      if (!isNaN(this.input('number').value)) {
+        logarithm = Math.log(this.input('number').value)
+      }
+      if (!isNaN(this.input('base').value)) {
+        logarithm /= Math.log(this.input('base').value)
+      }
+      this.output('logarithm').value = logarithm
+    }
   }
 }
 </script>
