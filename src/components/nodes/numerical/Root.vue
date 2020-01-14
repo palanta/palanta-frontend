3b13f9f6535307d9d4a2fdfa260225b1626de00a
Juli <juliane2109@gmail.com>
2020-01-14 19:55:23 +0100

    Implement Root node functionality

--- src/components/nodes/numerical/Root.vue
+++ src/components/nodes/numerical/Root.vue
@@ -11,20 +11,34 @@ export default {
     icon: require('../../../assets/node_icons/node_root.svg'),
     inputs: [
       {
+        id: 'number',
         name: 'Number',
         type: 'number'
       },
       {
+        id: 'degree',
         name: 'Degree',
         type: 'number'
       }
     ],
     outputs: [
       {
+        id: 'root',
         name: 'Root',
         type: 'number'
       }
-    ]
+    ],
+    calculate () {
+      let root = 0
+      let number = this.input('number').value
+      let degree = this.input('degree').value
+      if (!isNaN(number)) {
+        if (!isNaN(degree)) {
+          root = Math.pow(number, 1 / degree)
+        } else root = Math.sqrt(number)
+      }
+      this.output('root').value = root
+    }
   }
 }
 </script>
