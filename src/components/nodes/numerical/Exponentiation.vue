f61ca624b2035253a5eedd9ac3f445d05f783a89
Juli <juliane2109@gmail.com>
2020-01-14 19:09:06 +0100

    Implement Exponentiation node functionality

--- src/components/nodes/numerical/Exponentiation.vue
+++ src/components/nodes/numerical/Exponentiation.vue
@@ -11,20 +11,32 @@ export default {
     icon: require('../../../assets/node_icons/node_exponentiation.svg'),
     inputs: [
       {
+        id: 'number',
         name: 'Number',
-        type: 'number'
-      },
-      {
-        name: 'Exponent',
-        type: 'number'
+        type: 'number',
+        variadic: {
+          minimum: 2
+        }
       }
     ],
     outputs: [
       {
+        id: 'power',
         name: 'Power',
         type: 'number'
       }
-    ]
+    ],
+    calculate () {
+      let power = 1
+      const factors = this.input('number').map(factor => factor.value).filter(factor => !isNaN(factor))
+      if (factors.length > 1) {
+        power = factors.shift()
+        factors.forEach(exponent => {
+          power **= exponent
+        })
+      }
+      this.output('power').value = power
+    }
   }
 }
 </script>
