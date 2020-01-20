5c86bb2cadfab114c23549f492a6e516f8b8e85b
AKoll <31693992+AKoll@users.noreply.github.com>
2020-01-20 23:31:57 +0100

    Add app logo to main toolbar

--- src/layouts/MyLayout.vue
+++ src/layouts/MyLayout.vue
@@ -2,6 +2,9 @@
   <q-layout id="main-layout" view="lHh Lpr lFf">
     <q-header elevated>
       <q-toolbar>
+        <q-avatar>
+          <img src="../assets/palanta_logo_Manuscript1.svg" height="100%"/>
+        </q-avatar>
         <q-toolbar-title>
           Palanta
         </q-toolbar-title>
