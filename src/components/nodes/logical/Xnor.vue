ca25c8ce9c518f645301af12a24a302fc8b8e013
Juli <juliane2109@gmail.com>
2020-01-27 11:32:56 +0100

    Implement Xnor node functionality

--- src/components/nodes/logical/Xnor.vue
+++ src/components/nodes/logical/Xnor.vue
@@ -20,11 +20,26 @@ export default {
     ],
     outputs: [
       {
-        id: 'boolean',
-        name: 'Boolean',
+        id: 'result',
+        name: 'Result',
         type: 'boolean'
       }
-    ]
+    ],
+    calculate (input) {
+      const booleans = input.boolean.filter(booleans => !isNaN(booleans))
+      let result = true
+      if (booleans.length > 1) {
+        let bool1 = booleans.shift()
+        let bool2 = booleans.shift()
+        if (bool1 !== bool2) { result = false }
+        for (let bool of booleans) {
+          booleans.shift()
+          if (bool !== result) result = true
+          else result = false
+        }
+      }
+      return { result }
+    }
   }
 }
 </script>
