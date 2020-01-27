df11a90aceff37f02c3f4402fed31ed8d671c11a
AKoll <31693992+AKoll@users.noreply.github.com>
2020-01-27 14:42:41 +0100

    Remove accidental PNGroup import

--- src/components/nodes/miscellanious/miscellanious.js
+++ src/components/nodes/miscellanious/miscellanious.js
@@ -1,12 +1,10 @@
 import PNPreview from './Preview'
 import PNNote from './Note'
-import PNGroup from './Group'
 
 export default {
   basic: {
     PNPreview,
-    PNNote,
-    PNGroup
+    PNNote
   },
   advanced: {}
 }
